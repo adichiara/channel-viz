2b63137ccf490a87b524f00ee413b8e8e14beb4b
adichiara <adamdichiara@gmail.com>
2014-07-01 11:38:32 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var 	defaultKey	= 'QIwjfREYOb42dno1sqaRE32o1aqUHy3qFHPSArinB1hEuiOk', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [466998777], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Adam - Home Temperature', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor	= '', // CSS HEX value of color to represent data (omit leading #)
+		hideForm	= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
